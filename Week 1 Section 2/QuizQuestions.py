631231da1b30991c87b0ececbcf252cc38f3f823
Justice Vang <91295917+Imagineidot@users.noreply.github.com>
2025-09-12 10:54:26 -0500

    Add files via upload

---
+++ Week 1 Section 2/QuizQuestions.py
@@ -0,0 +1,126 @@
+# Written by Justice V
+
+'''
+# Question 1
+
+# Variables
+
+year = input("Enter Year: ")
+month = input("Enter Month: ")
+day = input("Enter Day: ")
+
+# Output
+
+print(f'The date is {month} {day}, {year}')
+'''
+'''
+# Question 2 
+
+# Variables
+
+name = input("Enter Name: ")
+age = input("Enter Age: ")
+
+# Output
+
+print(f'Hello {name}, you are getting old at {age} years old')
+'''
+'''
+# Question 3 
+
+# Variables
+
+first_name = input("Enter First Name: ")
+last_name = input("Enter Last Name: ")
+
+# Output
+
+print(f'Hello {first_name} {last_name}. See you later!')
+'''
+'''
+# Question 4
+
+# Variables
+
+first_name = input("Enter First Name: ")
+last_name = input("Enter Last Name: ")
+
+# Output
+
+print(f'Hello {first_name} {last_name}. How are you doing?')
+'''
+'''
+
+# Question 5
+
+# Variables
+
+color = input("Pick a color: ")
+animal = input("Pick a animal: ")
+
+# Output
+
+print(f'You have chosen a {color} {animal}')
+
+'''
+'''
+
+# Question 6
+
+# Variables
+
+food = input("Enter Food: ")
+drink = input("Enter Drink: ")
+
+# Output
+
+print(f'You have ordered a {food} with a {drink}')
+
+'''
+'''
+
+# Question 7 
+
+# Variables
+
+color = input("Favorite Color? ")
+food = input("Favorite Food? ")
+
+# Output
+
+print(f'I also love the color {color} and {food}!')
+
+'''
+
+# Question 8 
+
+#Variables
+
+city = input("Enter City: ")
+state = input("Enter State Related to City: ")
+
+# Output
+
+print(f'You want to go to {city}, {state}?')
+
+'''
+# Question 9
+
+# Variables
+
+x = 3
+y = 7
+
+
+print(x, y)
+
+z = x
+x=y
+y=z
+
+print(x, y)
+'''
+
+
+
+
